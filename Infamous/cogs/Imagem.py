6b9979904a26d577d6ca2f6d909130cd73b18b4e
Jay <36034467+OneEyedKnight@users.noreply.github.com>
2018-11-07 19:05:35 +0800

    added char limit on mind

--- Infamous/cogs/Imagem.py
+++ Infamous/cogs/Imagem.py
@@ -93,6 +93,9 @@ class Imagem:
     @commands.command()
     async def drake(self, ctx, user1: discord.Member, user2: discord.Member):
         """Compare two members using Drake."""
+        
+        if len(text1 or text2 or text3) > 30:
+            return await ctx.send("30 chars on each!")
 
         async with self.session.get(user1.avatar_url_as(format="png", size=512)) as r:
             av1 = await r.read()
