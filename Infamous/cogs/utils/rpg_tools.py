dca9a480b81136123f041aa8cd03ce83345cd00d
Jay <36034467+OneEyedKnight@users.noreply.github.com>
2018-12-08 19:01:28 +0800

    added tools for new rpg

--- Infamous/cogs/utils/rpg_tools.py
+++ Infamous/cogs/utils/rpg_tools.py
@@ -1,32 +1,36 @@
 import discord
 import asyncio
 
+embed_color = 0x0f0f0f
+
 
 async def lvl(ctx, mon, msg1, msg2, user=None):
     if not user:
         user = ctx.author.id
 
-    lvl_ = await ctx.bot.db.fetchrow(
-        "SELECT * FROM rpg_profile WHERE id=$1", user)
+    async with ctx.bot.db.acquire() as db:
+        lvl_ = await db.fetchrow(
+            "SELECT * FROM rpg_profile WHERE id=$1", user)
 
     if lvl_['xp'] >= lvl_['level'] * 50:
         await ctx.send(
             msg1
         )
-        await ctx.bot.db.execute(
-            "UPDATE rpg_profile SET level = $1 WHERE id=$2",
-            lvl_['level'] + 1, user
-        )
+        async with ctx.bot.db.acquire() as db:
+            await db.execute(
+                "UPDATE rpg_profile SET level = $1 WHERE id=$2",
+                lvl_['level'] + 1, user
+            )
 
-        await ctx.bot.db.execute(
-            "UPDATE rpg_profile SET xp = 0 WHERE id=$1",
-            user
-        )
+            await db.execute(
+                "UPDATE rpg_profile SET xp = 0 WHERE id=$1",
+                user
+            )
 
-        await ctx.bot.db.execute(
-            "UPDATE rpg_profile SET bal = bal + $1 WHERE id=$2",
-            mon, user
-        )
+            await db.execute(
+                "UPDATE rpg_profile SET bal = bal + $1 WHERE id=$2",
+                mon, user
+            )
     else:
         await ctx.send(msg2)
 
@@ -35,27 +39,29 @@ async def mastery_lvl(ctx, mon, skill, msg1, msg2, user=None):
     if not user:
         user = ctx.author.id
 
-    lvl_ = await ctx.bot.db.fetchrow(
-        "SELECT * FROM rpg_mastery WHERE id=$1 AND skill=$2", user, skill)
+    async with ctx.bot.db.acquire() as db:
+        lvl_ = await db.fetchrow(
+            "SELECT * FROM rpg_mastery WHERE id=$1 AND skill=$2", user, skill)
 
     if lvl_['xp'] >= lvl_['level'] * 50:
         await ctx.send(
             msg1
         )
-        await ctx.bot.db.execute(
-            "UPDATE rpg_mastery SET level = $1 WHERE id=$2 AND skill=$3",
-            lvl_['level'] + 1, user, skill
-        )
+        async with ctx.bot.db.acquire() as db:
+            await db.execute(
+                "UPDATE rpg_mastery SET level = $1 WHERE id=$2 AND skill=$3",
+                lvl_['level'] + 1, user, skill
+            )
 
-        await ctx.bot.db.execute(
-            "UPDATE rpg_mastery SET xp = 0 WHERE id=$1 AND skill=$2",
-            user, skill
-        )
+            await db.execute(
+                "UPDATE rpg_mastery SET xp = 0 WHERE id=$1 AND skill=$2",
+                user, skill
+            )
 
-        await ctx.bot.db.execute(
-            "UPDATE rpg_profile SET bal = bal + $1 WHERE id=$2",
-            mon, user
-        )
+            await db.execute(
+                "UPDATE rpg_profile SET bal = bal + $1 WHERE id=$2",
+                mon, user
+            )
     else:
         await ctx.send(msg2)
 
@@ -64,38 +70,42 @@ async def add_xp(ctx, xp, user=None):
     if not user:
         user = ctx.author.id
 
-    await ctx.bot.db.execute(
-        "UPDATE rpg_profile SET xp = xp + $1 WHERE id = $2",
-        xp, user
-    )
+    async with ctx.bot.db.acquire() as db:
+        await db.execute(
+            "UPDATE rpg_profile SET xp = xp + $1 WHERE id = $2",
+            xp, user
+        )
 
 
 async def add_mastery_xp(ctx, xp, skill, user=None):
     if not user:
         user = ctx.author.id
 
-    await ctx.bot.db.execute(
-        "UPDATE rpg_mastery SET xp = xp + $1 WHERE skill = $2 AND id = $3",
-        xp, skill, user
-    )
+    async with ctx.bot.db.acquire() as db:
+        await db.execute(
+            "UPDATE rpg_mastery SET xp = xp + $1 WHERE skill = $2 AND id = $3",
+            xp, skill, user
+        )
 
 
 async def add_money(ctx, mon, user=None):
     if not user:
         user = ctx.author
 
-    await ctx.bot.db.execute(
-        "UPDATE rpg_profile SET bal = bal + $1 WHERE id = $2",
-        mon, user.id
-    )
+    async with ctx.bot.db.acquire() as db:
+        await db.execute(
+            "UPDATE rpg_profile SET bal = bal + $1 WHERE id = $2",
+            mon, user.id
+        )
 
 
 async def fetch_user(ctx, user=None):
     if not user:
         user = ctx.author.id
 
-    p = await ctx.bot.db.fetchrow("SELECT * FROM rpg_profile WHERE id=$1",
-                                  user)
+    async with ctx.bot.db.acquire() as db:
+        p = await db.fetchrow("SELECT * FROM rpg_profile WHERE id=$1",
+                              user)
     return p
 
 
@@ -103,15 +113,16 @@ async def fetch_mastery(ctx, skill, user=None):
     if not user:
         user = ctx.author.id
 
-    p = await ctx.bot.db.fetchrow("SELECT * FROM rpg_mastery WHERE id=$1 AND skill=$2",
-                                  user, skill)
+    async with ctx.bot.db.acquire() as db:
+        p = await db.fetchrow("SELECT * FROM rpg_mastery WHERE id=$1 AND skill=$2",
+                              user, skill)
 
     return p
 
 
 def item_embed(item, thumbnail, current, max_):
-    embed = discord.Embed(color=0xba1c1c)
-    embed.set_author(name=f"{item[0]} | Price {item[2]}$" + f"Page {current} of {max_}".rjust(73))
+    embed = discord.Embed(color=embed_color)
+    embed.set_author(name=f"{item[0]} | Price {item[2]}$ | Page {current} of {max_}")
     embed.description = item[6]
     embed.add_field(name="Performance Stats", value=f"**Damage:** {item[3]} \n"
                                                     f"**Defense:** {item[4]}")
@@ -123,7 +134,7 @@ def item_embed(item, thumbnail, current, max_):
 
 
 async def lb_embed(ctx, pfp, current, max_):
-    embed = discord.Embed(color=0xba1c1c)
+    embed = discord.Embed(color=embed_color)
     member = ctx.bot.get_user(pfp[0])
     embed.set_author(name=member.name)
     embed.description = f"**Level:** {pfp[2]} \n" \
@@ -149,9 +160,11 @@ async def fetch_item(ctx, name, user=None, inv=None):
         user = ctx.author.id
 
     if inv == "rpg_shop":
-        item = await ctx.bot.db.fetchrow("SELECT * FROM rpg_shop WHERE name=$1", name)
+        async with ctx.bot.db.acquire() as db:
+            item = await db.fetchrow("SELECT * FROM rpg_shop WHERE name=$1", name)
     else:
-        item = await ctx.bot.db.fetchrow("SELECT * FROM rpg_inventory WHERE name=$1 AND owner=$2", name, user)
+        async with ctx.bot.db.acquire() as db:
+            item = await db.fetchrow("SELECT * FROM rpg_inventory WHERE name=$1 AND owner=$2", name, user)
 
     return item
 
@@ -160,13 +173,14 @@ async def remove_money(ctx, bal, user=None):
     if not user:
         user = ctx.author.id
 
-    await ctx.bot.db.execute("UPDATE rpg_profile SET bal = bal - $1 WHERE id=$2",
-                             bal, user)
+    async with ctx.bot.db.acquire() as db:
+        await db.execute("UPDATE rpg_profile SET bal = bal - $1 WHERE id=$2",
+                         bal, user)
 
 
 def inventory_embed(ctx, info, thumbnail, current, max_):
-    embed = discord.Embed(color=0xba1c1c)
-    embed.set_author(name=f"{ctx.bot.get_user(info[7]).name}'s inventory" + f"Page {current} of {max_}".rjust(20),
+    embed = discord.Embed(color=embed_color)
+    embed.set_author(name=f"{ctx.bot.get_user(info[7]).name}'s inventory | Page {current} of {max_}",
                      icon_url=ctx.bot.get_user(info[7]).avatar_url)
     embed.description = (f"**Name:** {info[0]} \n"
                          f"**Price:** {info[2]} \n"
@@ -182,7 +196,8 @@ async def fetch_skills(ctx, user=None):
     if not user:
         user = ctx.author
 
-    skills = await ctx.bot.db.fetch("SELECT * FROM rpg_mastery WHERE id=$1", user.id)
+    async with ctx.bot.db.acquire() as db:
+        skills = await db.fetch("SELECT * FROM rpg_mastery WHERE id=$1", user.id)
 
     p = []
     for i in skills:
@@ -230,9 +245,122 @@ async def lb(ctx, win, loss, user=None):
 
     data = await ctx.bot.db.fetch("SELECT * FROM rpg_duels WHERE id=$1", user.id)
     if data:
-        await ctx.bot.db.execute("""UPDATE rpg_duels 
-                                    SET wins = wins + $1, losses = losses + $2 
-                                    WHERE id=$3
-                                """, win, loss, user.id)
+        async with ctx.bot.db.acquire() as db:
+            await db.execute("""UPDATE rpg_duels 
+                                        SET wins = wins + $1, losses = losses + $2 
+                                        WHERE id=$3
+                                    """, win, loss, user.id)
     else:
-        await ctx.bot.db.execute("INSERT INTO rpg_duels VALUES($1, $2, $3)", user.id, win, loss)
+        async with ctx.bot.db.acquire() as db:
+            await db.execute("INSERT INTO rpg_duels VALUES($1, $2, $3)", user.id, win, loss)
+
+
+async def level2(ctx, mon, xp, user=None):
+    if not user:
+        user = ctx.author
+
+    async with ctx.bot.db.acquire() as db:
+        lvl_ = await db.fetchrow("SELECT * FROM profiles WHERE id=$1", user.id)
+
+    lvl_ = {"xp": lvl_[2] + xp, "lvl": lvl_[1]}
+
+    if lvl_["xp"] >= lvl_["lvl"] * 2000:
+        await ctx.send(f"Congratulations {user.mention} you have leveled up to Level {lvl_['lvl'] + 1}.")
+
+        async with ctx.bot.db.acquire() as db:
+            await db.execute("UPDATE profiles SET level = level + 1, bal = bal + $1, xp = xp + $2 WHERE id=$3",
+                             mon, xp, user.id)
+    else:
+        await ctx.send(f"{user.mention} You have {lvl_['lvl'] * 2000 - lvl_['xp']}xp left to the next level.")
+
+        async with ctx.bot.db.acquire() as db:
+            await db.execute("UPDATE profiles SET bal = bal + $1, xp = xp + $2 WHERE id=$3",
+                             mon, xp, user.id)
+
+
+async def fetch_user2(ctx, user=None):
+    if not user:
+        user = ctx.author
+
+    async with ctx.bot.db.acquire() as db:
+        info = await db.fetchrow("SELECT * FROM profiles WHERE id=$1", user.id)
+    return info
+
+
+async def fetch_abilities(ctx, user=None):
+    if not user:
+        user = ctx.author
+    p = []
+
+    async with ctx.bot.db.acquire() as db:
+        abilities = await db.fetch("SELECT * FROM abilities WHERE id=$1", user.id)
+
+    for i in abilities:
+        p.append(i[1])
+    return p
+
+
+def ability_embed(ctx, dict_, ability, current, max_):
+    embed = discord.Embed(color=embed_color)
+    embed.set_author(name=f"{ability} | ${dict_[ability][0]}")
+    embed.description = dict_[ability][2]
+    embed.add_field(name="Stats", value=f"**Damage:** {dict_[ability][3]} \n"
+                                        f"**Durability:** {dict_[ability][4]}")
+    embed.set_image(url=dict_[ability][1])
+    embed.set_footer(text=f'Page {current} of {max_} | Use "{ctx.prefix}acquire <ability>" to get this ability')
+    return embed
+
+
+async def ability_level(ctx, xp, dmg, dur, ability, user=None):
+    if not user:
+        user = ctx.author
+
+    async with ctx.bot.db.acquire() as db:
+        lvl_ = await db.fetchrow("SELECT * FROM abilities WHERE id=$1 AND ability=$2", user.id, ability)
+
+    lvl_ = {"lvl": lvl_[2], "xp": lvl_[3] + xp}
+
+    if lvl_["xp"] >= lvl_["lvl"] * 2000:
+        await ctx.send(f"Congratulations {user.mention} you have leveled up {ability} to Level {lvl_['lvl'] + 1}.")
+
+        async with ctx.bot.db.acquire() as db:
+            await db.execute(
+                "UPDATE abilities SET level = level + 1, damage=damage + $1, "
+                "durability=durability + $2, xp = xp + $3 WHERE id=$4 AND ability=$5",
+                dmg, dur, xp, user.id, ability)
+    else:
+        await ctx.send(
+            f"{user.mention} You have {lvl_['lvl'] * 2000 - lvl_['xp']}xp left to upgrade your "
+            f"{ability} to the next level.")
+
+        async with ctx.bot.db.acquire() as db:
+            await db.execute("UPDATE abilities SET damage=damage + $1, "
+                             "durability=durability + $2, xp = xp + $3 WHERE id=$4 AND ability=$5",
+                             dmg, dur, xp, user.id, ability)
+
+
+async def guild_level(ctx, xp, user=None):
+    if not user:
+        user = ctx.author
+
+    async with ctx.bot.db.acquire() as db:
+        lvl_ = await db.fetchval("SELECT guild FROM profiles WHERE id=$1", user.id)
+        lvl_ = await db.fetchrow("SELECT * FROM guilds WHERE guild=$1", lvl_)
+
+    if not lvl_:
+        return await ctx.send("You currently aren't apart of a guild; therefore there are no guild rewards.")
+    else:
+        lvl_ = {"xp": lvl_[2] + xp, "lvl": lvl_[1], "name": lvl_[0]}
+
+    if lvl_["xp"] >= lvl_["lvl"] * 2000:
+        await ctx.send(f"{lvl_['name']} has leveled up to Level {lvl_['lvl'] + 1}.")
+
+        async with ctx.bot.db.acquire() as db:
+            await db.execute("UPDATE guild SET level = level + 1, xp = xp + $1 WHERE id=$2",
+                             xp, lvl_['name'])
+    else:
+        await ctx.send(f"**{lvl_['name']}** needs {lvl_['lvl'] * 2000 - lvl_['xp']}xp left to the next level.")
+
+        async with ctx.bot.db.acquire() as db:
+            await db.execute("UPDATE guilds SET xp = xp + $1 WHERE id=$2",
+                             xp, user.id)
